2e0ab9a3ace06aa365212529cf7d2c62fd39a32b
TM1610 <tan1610maygmail.com>
2025-07-25 10:07:47 +0530

    Strings

---
+++ String1.java
@@ -0,0 +1,16 @@
+public class String1 {
+    public static void main(String[] args) {
+     // this is what we basicaly do to print string
+     
+        String str = "Hello, World!";
+        System.out.println(str);
+
+
+        // Cretaing an Object of an String class
+        String str1= new String("Tanmay");
+        System.out.println("HEllo"+str1);
+
+        //String CharAt
+        System.out.println(str1.charAt(1));//print a as at index 1 there is letter a
+    }
+}
